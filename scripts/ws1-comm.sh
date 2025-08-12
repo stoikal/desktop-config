f9c175a239c947e09c4b650d6452ac497576e83a
Laksito Pamilih <laksito@protonmail.com>
2025-08-12 13:49:54 +0700

    ws 2

--- scripts/ws1-comm.sh
+++ scripts/ws1-comm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Open Firefox in i3 workspace 1
 
-i3-msg workspace number 1
-firefox --new-window  https://web.whatsapp.com/ &
+
+# Set workspace name
+WS=1
+WS_NAME="$WS.comm"
+i3-msg workspace "$WS_NAME"
+i3-msg rename workspace to "$WS_NAME"
+
+firefox --new-window https://web.whatsapp.com/ &
 firefox --new-window https://gemini.google.com/ &
